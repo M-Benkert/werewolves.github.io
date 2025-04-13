9ce3917a6a2e9d2d29ab258ebeadac7110c48404
Maximilian Benkert <benkert.maximilian@web.de>
2025-04-13 16:12:15 +0200

    Prune framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<h1>{{ title }}</h1><router-outlet />`,
+  styles: [],
 })
 export class AppComponent {
   title = 'werewolves.github.io';
